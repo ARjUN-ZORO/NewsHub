234ac854be99708354b2ce282e20446f5befd8e9
ARjUN-ZORO <zoro.arjun@gmail.com>
2020-05-02 19:42:19 +0530

    Page_by_cat Added resource and a end point /api/news?cat=&page=

--- newshub/api/main.py
+++ newshub/api/main.py
@@ -1,8 +1,9 @@
 from flask_restful import Api
 
 from .db import db
-from .resources import Page
+from .resources import Page, Page_by_cat
 
 api = Api()
 
 api.add_resource(Page, '/api/latest_news')
+api.add_resource(Page_by_cat, '/api/news')
