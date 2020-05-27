2b1b12464e2d53dabdbb9c0d0e16df380a7e391c
ARjUN-ZORO <zoro.arjun@gmail.com>
2020-05-28 01:14:42 +0530

    date place in lazy

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from newshub import create_app
 app = create_app()
 
 if __name__ == '__main__':
-	app.run(debug=True, port=5000,threaded=True,host='0.0.0.0')
+	app.run(debug=True, port=5000, threaded=True)
